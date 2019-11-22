6d5a336e596c7b7742254605151181f4c5e91f35
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-21 19:47:32 -0800

    final functionality determined

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+function printLyrics(lyrics, artist, song) {
+    console.log(lyrics);
+    $('.js-lyrics').html(`<h2>${song} by ${artist}</h2><pre>${lyrics.lyrics}</pre>`);
+}
+
+function printInfo(info) {
+    $('.js-info').html(`
+    <button class="js-exit">X</button>
+    <img src="${info.strTrackThumb}">
+    <h3>${info.strAlbum}</h3>
+    <pre>${info.strDescriptionEN}</pre>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${formatLink(info.strMusicVid)}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p>Directed by: ${info.strMusicVidDirector}</p>
+    <p>Genre: ${info.strStyle}</p>
+    `);
+}
+
+function showInfo() {
+    console.log('info');
+    if($('.js-modal').css( "display" ) == 'none') {
+        $('.js-modal').css('display', 'block');
+    }
+    else {
+        $('.js-modal').css('display', 'none');
+    }
+    
+}
+
+function formatLink(link) {
+    return link.slice(31);
+}
+
 function formatSearch(query) {
     return query.trim().toLowerCase()
 }
 
 function formatDataSearch(params) {
     return Object.keys(params).map(key => `${key.trim().replace(/ /g, '_').toLowerCase()}=${(params[key]).trim().replace(/ /g, '_').toLowerCase()}`).join('&');
-}
-
-function printLyrics(lyrics) {
-    console.log(lyrics);
-    $('.js-lyrics').html(`<pre>${lyrics.lyrics}</pre>`);
 }
 
 function fetchLyrics(artist, song) {
@@ -24,7 +51,7 @@ function fetchLyrics(artist, song) {
         })
         .then(lyrics => lyrics.json())
         .then(jsonLyrics => {
-            printLyrics(jsonLyrics);
+            printLyrics(jsonLyrics, artist, song);
         })
         .catch(error => {
             alert('Something went wrong...');
@@ -40,16 +67,24 @@ function fetchSongDetails(artist, song) {
     let queries = formatDataSearch(params);
     const url = `https://theaudiodb.com/api/v1/json/1/searchtrack.php?${queries}`;
     fetch(url)
-        .then(lyrics => lyrics.json())
-        .then(jsonLyrics => {
-            console.log(jsonLyrics.track[0].strDescriptionEN);
-            console.log(jsonLyrics.track[0].strMusicVidDirector);
-            console.log(jsonLyrics.track[0].strMusicVid);
-            console.log(jsonLyrics.track[0].strTrackThumb);
-            console.log(jsonLyrics.track[0].strAlbum);
-            console.log(jsonLyrics.track[0].strGenre);
-            console.log(jsonLyrics.track[0].strStyle);
+        .then(info => info.json())
+        .then(jsonInfo => {
+            printInfo(jsonInfo.track[0]);
         });
+}
+
+function handleInfoClick() {
+    $('.js-info-button').on('click', function (event) {
+        event.preventDefault();
+        showInfo();
+    });
+}
+
+function handleExitClick() {
+    $('.js-info').on('click', function (event) {
+        event.preventDefault();
+        showInfo();
+    });
 }
 
 function handleSearch() {
@@ -65,6 +100,10 @@ function handleSearch() {
 
 function main() {
     handleSearch();
+    handleExitClick();
+    handleInfoClick();
+    fetchLyrics('queen', 'bohemian rhapsody');
+    fetchSongDetails('queen', 'bohemian rhapsody');
 }
 
 $(main);
